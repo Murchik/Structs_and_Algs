11a3b262dbfa42d8d31a17efe292b39a9f79a3ab
Aleksandr Kasatkin <alexkasatkin90@gmail.ru>
2020-02-24 01:12:55 +0300

    Code redestribution

--- BinarySearchTree/tree.cpp
+++ BinarySearchTree/tree.cpp
@@ -1,65 +1,176 @@
-#include <iostream>
 #include "tree.hpp"
 
-int main()
+Node* BinTree::search(int key)
 {
-    using namespace std;
-
-    BinTree tree;
-    Node *rez;
-
-    tree.insert(9);
-    tree.insert(11);
-    tree.insert(7);
-    tree.insert(8);
-    tree.insert(10);
-    tree.insert(6);
-
-    if ((rez = tree.search(6)) != nullptr)
-        cout << "Found element: " << rez->data << endl;
-    else
-        cout << "Element " << 6 << " not found" << endl;
-
-    if ((rez = tree.search(1)) != nullptr)
-        cout << "Found element: " << rez->data << endl;
-    else
-        cout << "Element " << 1 << " not found" << endl;
-
-    cout << endl;
-
-    rez = tree.getRoot();
-    cout << "Tree pre-traversal here:\n ";
-    tree.preTraversal(rez);
-    cout << endl;
-
-    rez = tree.getRoot();
-    cout << "Tree in-traversal here:\n ";
-    tree.inTraversal(rez);
-    cout << endl;
-
-    rez = tree.getRoot();
-    cout << "Tree post-traversal here:\n ";
-    tree.postTraversal(rez);
-    cout << endl;
-
-    cout << endl;
-
-    rez = tree.min();
-    cout << "min element: " << rez->data << endl;
-
-    rez = tree.max();
-    cout << "max element: " << rez->data << endl;
-
-    cout << endl;
-    
-    for (int i = 7; i < 11; ++i)
+    Node* current;
+    current = root;
+    while (current != nullptr && current->data != key)
     {
-        rez = tree.next(i);
-        cout << "next element for "<< i <<": " << rez->data << endl;
-        rez = tree.prev(i);
-        cout << "prev element for "<< i <<": " << rez->data << endl;
-        cout << endl;
+        if (key < current->data)
+            current = current->left;
+        else
+            current = current->right;
+    }
+    return current;
+}
+
+void BinTree::preTraversal(Node* current)
+{
+    if (current == nullptr)
+        return;
+    std::cout << current->data << " ";
+    preTraversal(current->left);
+    preTraversal(current->right);
+}
+
+void BinTree::inTraversal(Node* current)
+{
+    if (current == nullptr)
+        return;
+    inTraversal(current->left);
+    std::cout << current->data << " ";
+    inTraversal(current->right);
+}
+
+void BinTree::postTraversal(Node* current) const
+{
+    if (current == nullptr)
+        return;
+    postTraversal(current->left);
+    postTraversal(current->right);
+    std::cout << current->data << " ";
+}
+
+Node* BinTree::min()
+{
+    Node* current = root;
+    while (current->left != nullptr)
+        current = current->left;
+    return current;
+}
+
+Node* BinTree::min(Node* current)
+{
+    if (current != nullptr)
+    {
+        while (current->left != nullptr)
+            current = current->left;
+    }
+    return current;
+}
+
+Node* BinTree::max()
+{
+    Node* current = root;
+    while (current->right != nullptr)
+        current = current->right;
+    return current;
+}
+
+Node* BinTree::max(Node* current)
+{
+    if (current != nullptr)
+    {
+        while (current->right != nullptr)
+            current = current->right;
+    }
+    return current;
+}
+
+/* TODO:
+     Не учтён случай, когда элемента для которого
+    требуется найти следующий не существует.
+     Не учтён случай, когда для заданного элемента
+    не существует следующего.
+*/
+Node* BinTree::next(int key)
+{
+    Node* current = search(key);
+    Node* parent = current->parent;
+    if (current->right != nullptr)
+    {
+        current = min(current->right);
+        return current;
     }
 
-    return 0;
+    if (current == parent->left)
+        return parent;
+
+    while (current != parent->left)
+    {
+        current = current->parent;
+        parent = parent->parent;
+    }
+    return current->parent;
+}
+
+/* TODO:
+     Не учтён случай, когда элемента для которого
+    требуется найти предыдущий не существует.
+     Не учтён случай, когда для заданного элемента
+    не существует предыдущего.
+*/
+Node* BinTree::prev(int key)
+{
+    Node* current = search(key);
+    Node* parent = current->parent;
+    if (current->left != nullptr)
+    {
+        current = max(current->left);
+        return current;
+    }
+
+    if (current == parent->right)
+        return parent;
+
+    while (current != parent->right)
+    {
+        current = current->parent;
+        parent = parent->parent;
+    }
+    return current->parent;
+}
+
+void BinTree::insert(int value)
+{
+    Node* newElement;
+    Node* current;
+    Node* parent;
+
+    current = root;
+
+    if (current == nullptr)
+    {
+        newElement = new Node(value);
+        root = newElement;
+        return;
+    }
+
+    do
+    {
+        if (value > current->data)
+        {
+            parent = current;
+            current = current->right;
+        }
+        else
+        {
+            parent = current;
+            current = current->left;
+        }
+
+    } while (current != nullptr);
+
+    newElement = new Node(value);
+    newElement->parent = parent;
+
+    if (value < parent->data)
+        parent->left = newElement;
+    else
+        parent->right = newElement;
+}
+
+void erase(int key)
+{
+    // INSERT YOUR CODE HERE
 }
--- BinarySearchTree/tree.hpp
+++ BinarySearchTree/tree.hpp
@@ -26,7 +26,8 @@ struct Node
     }
     ~Node()
     {
-        // INSERT YOUR CODE HERE
+        delete left;
+        delete right;
     }
 };
 
@@ -34,23 +35,17 @@ class BinTree
 {
 private:
     Node *root;
-    int count;
+    
 
 public:
-    BinTree()
-    {
-        root = nullptr;
-        count = 0;
-    }
+    BinTree() : root(nullptr) {};
+   
     ~BinTree()
     {
-        // INSERT YOUR CODE HERE
+        delete root;
     }
 
-    int getCount()
-    {
-        return count;
-    }
+    
     Node *getRoot()
     {
         return root;
@@ -70,7 +65,7 @@ public:
     void inTraversal(Node *);
 
     // (*) Обратный обход дерева (Post-order / LRN)
-    void postTraversal(Node *);
+    void postTraversal(Node *) const;
 
     // Поиск минимального элемента в дереве (или в поддереве)
     Node *min();
@@ -95,179 +90,6 @@ public:
 
 // ========================================================================= //
 
-Node *BinTree::search(int key)
-{
-    Node *current;
-    current = root;
-    while (current != nullptr && current->data != key)
-    {
-        if (key < current->data)
-            current = current->left;
-        else
-            current = current->right;
-    }
-    return current;
-}
 
-void BinTree::preTraversal(Node *current)
-{
-    if (current == nullptr)
-        return;
-    std::cout << current->data << " ";
-    preTraversal(current->left);
-    preTraversal(current->right);
-}
-
-void BinTree::inTraversal(Node *current)
-{
-    if (current == nullptr)
-        return;
-    inTraversal(current->left);
-    std::cout << current->data << " ";
-    inTraversal(current->right);
-}
-
-void BinTree::postTraversal(Node *current)
-{
-    if (current == nullptr)
-        return;
-    postTraversal(current->left);
-    postTraversal(current->right);
-    std::cout << current->data << " ";
-}
-
-Node *BinTree::min()
-{
-    Node *current = root;
-    while (current->left != nullptr)
-        current = current->left;
-    return current;
-}
-
-Node *BinTree::min(Node *current)
-{
-    if (current != nullptr)
-    {
-        while (current->left != nullptr)
-            current = current->left;
-    }
-    return current;
-}
-
-Node *BinTree::max()
-{
-    Node *current = root;
-    while (current->right != nullptr)
-        current = current->right;
-    return current;
-}
-
-Node *BinTree::max(Node *current)
-{
-    if (current != nullptr)
-    {
-        while (current->right != nullptr)
-            current = current->right;
-    }
-    return current;
-}
-
-/* TODO: 
-     Не учтён случай, когда элемента для которого
-    требуется найти следующий не существует.
-     Не учтён случай, когда для заданного элемента
-    не существует следующего.
-*/
-Node *BinTree::next(int key)
-{
-    Node *current = search(key);
-    Node *parent = current->parent;
-    if (current->right != nullptr)
-    {
-        current = min(current->right);
-        return current;
-    }
-
-    if (current == parent->left)
-        return parent;
-
-    while (current != parent->left)
-    {
-        current = current->parent;
-        parent = parent->parent;
-    }
-    return current->parent;
-}
-
-/* TODO: 
-     Не учтён случай, когда элемента для которого
-    требуется найти предыдущий не существует.
-     Не учтён случай, когда для заданного элемента
-    не существует предыдущего.
-*/
-Node *BinTree::prev(int key)
-{
-    Node *current = search(key);
-    Node *parent = current->parent;
-    if (current->left != nullptr)
-    {
-        current = max(current->left);
-        return current;
-    }
-
-    if (current == parent->right)
-        return parent;
-
-    while (current != parent->right)
-    {
-        current = current->parent;
-        parent = parent->parent;
-    }
-    return current->parent;
-}
-
-void BinTree::insert(int value)
-{
-    Node *newElement;
-    Node *current;
-    Node *parent;
-
-    current = root;
-    count++;
-    if (current == nullptr)
-    {
-        newElement = new Node(value);
-        root = newElement;
-        return;
-    }
-
-    do
-    {
-        if (value > current->data)
-        {
-            parent = current;
-            current = current->right;
-        }
-        else
-        {
-            parent = current;
-            current = current->left;
-        }
-
-    } while (current != nullptr);
-
-    newElement = new Node(value);
-    newElement->parent = parent;
-
-    if (value < parent->data)
-        parent->left = newElement;
-    else
-        parent->right = newElement;
-}
-
-void erase(int key)
-{
-    // INSERT YOUR CODE HERE
-}
 
 #endif
