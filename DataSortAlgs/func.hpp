4ea06dad876c7b237e3ca084196b06bed96832be
Aleksandr Kasatkin <alexkasatkin90@gmail.ru>
2020-04-11 14:58:59 +0300

    Added test sort & counter for shaker

--- DataSortAlgs/func.hpp
+++ DataSortAlgs/func.hpp
@@ -8,14 +8,16 @@
 std::vector<int> readVec();
 
 template <class Arr>
-void printVec(const Arr& input) {
+void printVec(const Arr& input) 
+{
     std::copy(input.begin(), input.end(),
               std::ostream_iterator<typename Arr::value_type>(std::cout, " "));
     std::cout << std::endl;
 }
 
 template <class Arr>
-void shakeSort(Arr input) {
+void shakeSort(Arr &input) 
+{
     std::cout << "Shake sort: " << std::endl;
     double compCount = 0;
     auto startTimer = std::chrono::high_resolution_clock::now();
@@ -23,29 +25,38 @@ void shakeSort(Arr input) {
     int start = 0;
     int end = input.size() - 1;
     bool swapped = true;
-    while (swapped) {
+    while (swapped) 
+    {
         swapped = false;
 
-        for (int i = start; i < end; ++i) {
-            if (input[i] > input[i + 1]) {
+        for (int i = start; i < end; ++i) 
+        {
+            if (input[i] > input[i + 1]) 
+            {
                 std::swap(input[i], input[i + 1]);
                 swapped = true;
             }
+            ++compCount;
         }
-        if (!swapped) {
+        if (!swapped) 
             break;
-        }
+        
 
         swapped = false;
         --end;
 
-        for (int i = end - 1; i >= start; --i) {
-            if (input[i] > input[i + 1]) {
+        for (int i = end - 1; i >= start; --i) 
+        {
+            if (input[i] > input[i + 1]) 
+            {
                 std::swap(input[i], input[i + 1]);
                 swapped = true;
             }
+            ++compCount;
         }
         ++start;
+
+
     }
 
     auto endTimer = std::chrono::high_resolution_clock::now();
@@ -54,10 +65,13 @@ void shakeSort(Arr input) {
     std::cout << "Comparisons: " << compCount << std::endl
               << "Time spend: " << duration.count() << "s" << std::endl
               << std::endl;
+
+    
 }
 
 template <class Arr>
-void shellSort(Arr array) {
+void shellSort(Arr &array) 
+{
     std::cout << "Shell sort: " << std::endl;
     int size = array.size();
     double compCount = 0;
@@ -68,9 +82,8 @@ void shellSort(Arr array) {
     for (h = size / 2; h > 0; h /= 2)
         for (i = h; i < size; i++)
             for (j = i - h; j >= 0 && array[j] > array[j + h];
-                 j -= h, ++compCount) {
-                                std::swap(array[j], array[j + h]);
-            }
+                j -= h, ++compCount)
+                std::swap(array[j], array[j + h]);
 
     auto endTimer = std::chrono::high_resolution_clock::now();
 
--- DataSortAlgs/main.cpp
+++ DataSortAlgs/main.cpp
@@ -2,22 +2,60 @@
 
 #define MAXELEM 10000
 
-int main() {
+int main() 
+{
+    std::vector<int> test(10);
+
+    for (size_t i = 0; i < 10; ++i)
+    {
+        test[i] = rand() % 1000;
+    }
+
+    std::cout << "A test on both sorts" << std::endl << std::endl;
+    std::cout << "Initial array: ";
+    printVec(test);
+    shakeSort(test);
+    std::cout << "Array after sort: ";
+    printVec(test);
+    std::cout << std::endl;
+    for (size_t i = 0; i < 10; ++i)
+    {
+        test[i] = rand() % 1000;
+    }
+    std::cout << "Initial array: ";
+    printVec(test);
+
+    shellSort(test);
+
+    std::cout << "Array after sort: ";
+    std::cout << std::endl;
+    printVec(test);
+    std::cout << std::endl;
+
+    std::cout << "A test on a 10000 element array" << std::endl;
+
     std::vector<int> vec(MAXELEM);
 
-    for (size_t i = 0; i < MAXELEM; ++i) {
+    for (size_t i = 0; i < MAXELEM; ++i) 
+    {
         vec[i] = rand() % 1000;
     }
 
-    //printVec(vec);
+    
 
     shakeSort(vec);
+    
 
-    for (size_t i = 0; i < MAXELEM; ++i) {
+    for (size_t i = 0; i < MAXELEM; ++i)
+    {
         vec[i] = rand() % 1000;
     }
+    
+    
 
     shellSort(vec);
+
+
 
     return 0;
 }
